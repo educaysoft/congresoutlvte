5745f1b9ec1bf9883aefc11e7f10db9a5d50fc8e
Stain Francis <educaysoft@gmail.com>
2022-08-02 02:23:37 -0400

    media query

--- carrusel.php
+++ carrusel.php
@@ -19,18 +19,38 @@
   </div>
 </div>
 
+<div class="card" style="width: 18rem;">
+  <video controls="" src="https://congresoutlvte.org/faci/video/rector.mp4"
+    class="bs-card-video"></video>
+  <div class="card-body">
+    <h5 class="card-title">Rector de la UTELVT</h5>
+    <p class="card-text">
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+<div class="card" style="width: 18rem;">
+  <video controls="" src="https://congresoutlvte.org/faci/video/rector.mp4"
+    class="bs-card-video"></video>
+  <div class="card-body">
+    <h5 class="card-title">Rector de la UTELVT</h5>
+    <p class="card-text">
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+
 
 </div>
 
 
 <!---- <center><a href="https://docs.google.com/forms/d/e/1FAIpQLSeQiGNyzm0penp8g2X1UDPCyhsgVFpSZ-CZI8YTO6gITv-wfQ/viewform?usp=sf_link"><button class="button-33" role="button">Inscribete AQUI</button></a> ---> 
 
-<div style="padding:20px 10% 10px 10%; justify-content: center; " >
 
-
-<center><a href="https://forms.gle/JBN4QAV7ryW3pVf28"><button class="button-33" role="button">Inscribete AQUÍ*</button></a>&nbsp;  &nbsp;<a href="#fsaludo"><button class="button-33" role="button">Ver más saludos</button></a>&nbsp;  &nbsp;<a href="https://congresoutlvte.org/facped/congresoutlvte-confe2.php"><button class="button-33" role="button">Los conferencistas</button></a></center>
-<br>
-		<p style="font-size: 1vw; text-align: center;font-weight: bold;">  <a href="mailto:congresoutlvte@utelvt.edu.ec">congresoutlvte@utelvt.edu.ec</a>
-		<br><a href="http://www.facebook.com/facultaddelapedagogia.facped">http://www.facebook.com/facultaddelapedagogia.facped</a> </p>
-</div>
 </div>
