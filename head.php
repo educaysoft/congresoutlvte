0200b52f724b8867b3bcb61d01723795b7f50230
Stain Francis <educaysoft@gmail.com>
2022-08-31 00:37:42 -0400

    mejoras

--- head.php
+++ head.php
@@ -3,18 +3,20 @@
 
 <main style="width:100%">
 
-<nav class="navbar navbar-dark bg-dark" aria-label="Dark offcanvas navbar">
+
+
+<nav class="navbar bg-light" aria-label="Light offcanvas navbar">
     <div class="container-fluid">
- <a class="navbar-brand" href="#">
+<a class="navbar-brand" href="#">
       <img src="https://congresoutlvte.org/images/logoutlvte.png" alt="..." height="36">
     </a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbarDark" aria-controls="offcanvasNavbarDark">
+      <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbarLight" aria-controls="offcanvasNavbarLight">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasNavbarDark" aria-labelledby="offcanvasNavbarDarkLabel">
+      <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbarLight" aria-labelledby="offcanvasNavbarLightLabel">
         <div class="offcanvas-header">
-          <h5 class="offcanvas-title" id="offcanvasNavbarDarkLabel">Offcanvas</h5>
-          <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          <h5 class="offcanvas-title" id="offcanvasNavbarLightLabel">Offcanvas</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
         <div class="offcanvas-body">
           <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
@@ -46,6 +48,7 @@
       </div>
     </div>
   </nav>
+
 
 
 
