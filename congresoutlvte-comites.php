6ac05905115b6cc38bdcaa58d13d3b36bb4bc4d8
Stain Francis <educaysoft@gmail.com>
2022-08-31 22:11:30 -0400

    mejoras

---
+++ congresoutlvte-comites.php
@@ -0,0 +1,259 @@
+<div id="equipo" style="border-top: 2px solid green;    width: 90%; margin:20px auto; display: inline-block; ";
+<div id="xxx" class="w3-container" style=" margin-left:10px; margin-top:10px;  padding: 0px; width: 100%; height: 20vh; display: inline-block; ">
+
+<div style="float:left; width: 20%; font-size: 0.5wv;">
+  <header>
+    <p style="margin-bottom: 10px; margin-top:10px;"><b>Miembros de Honor</b></p>
+  </header>
+  <main>
+
+  <b>Rector de la UTLVTE</b><br>
+  <p class="nombre">PhD. Girald David Vernaza Arroyo.</p> <br>
+
+
+  <b>Vice-Rectora de la UTLVTE</b><br><br>
+  <p class="nombre">PhD. María Elizabeth Canchingre Bone.</p> 
+
+
+
+  <b>Vice-Rector de Investigación y Postgrado</b><br><br>
+  <p class="nombre">PhD. Manuel Quiñonez.</p> 
+
+
+  <b>Decano de la FACI-UTLVTE</b><br><br>
+  <p class="nombre">M.Sc. Lenin Montaño Roldan.</p> 
+
+
+
+  <b>Coordinador del congreso</b><br><br>
+  <p class="nombre">Ing. Franklin Ochoa.</p> 
+  </main>
+</div>
+
+<div style="float:left; width: 20%;">
+  <header>
+    <p  style="margin-bottom: 10px; margin-top:10px;" ><b>COMISIÓN CIENTÍFICO</b></p>
+  </header>
+  <main>
+  <p class="nombre">PhD. Elizabeth Canchingre.</p>
+  <p>Vicerrectora Académica.</p><br>
+  <p class="nombre">PhD. Manuel Quiñonez Cabezas.<br>
+  Vicerrector de Investigación.</p><br><br>
+  
+  <p class="nombre">PhD. Juan Tacoronte.<br>
+  Docente FACI.</p><br>
+
+  <p class="nombre">PhD. Rugina Quiñonez.<br>
+  Docente FACI.</p><br>
+
+  <p class="nombre">PhD. Maribel Cuello.<br>
+  Directora carrera Ing. Química.</p><br>
+
+
+  <p class="nombre">PhD. Guadalupe Gómez.<br>
+  Docente FACI.</p><br>
+
+
+
+  <p class="nombre">PhD. Eduardo Rivas.<br>
+  Docente FACI.</p><br>
+
+
+  <p class="nombre">Ing. Guillermo Mosquera.<br>
+  Docente FACI.</p><br>
+
+
+  <p class="nombre">Ing. Lenin Montaño MsC.<br>
+  Docente FACI.</p><br>
+
+
+  <p class="nombre">Ing. Lenin Montaño MsC.<br>
+  Docente FACI.</p><br>
+
+  <p class="nombre">Ing. Diego Hurtado MsC.<br>
+  Docente FACI.</p><br>
+
+  <p class="nombre">Ing. Joseph Cruel MsC.<br>
+  Docente FACI.</p><br>
+
+
+  <p class="nombre">Ing. Damian Perez  MsC.<br>
+  Docente FACI.</p><br>
+
+
+  <p class="nombre">Ing. Leopoldo Gracia  MsC.<br>
+  Docente FACI.</p><br>
+
+  <p class="nombre">Ing. Franklin Ochoa  MsC.<br>
+  Docente FACI.</p><br>
+
+
+  <p class="nombre">Ing. Rafael Bastidas  MsC.<br>
+  Docente FACI.</p><br>
+
+
+  <p class="nombre">Ing. David Olmedo  MsC.<br>
+  Docente FACI.</p><br>
+
+
+  <p class="nombre">Ing. Silvia Simisterra  MsC.<br>
+  Docente FACI.</p><br>
+
+  <p class="nombre">Ing. Marcelo Reina  MsC.<br>
+  Docente FACI.</p><br>
+
+
+
+  <p class="nombre">Ing. Baster Ly Estupiñan  MsC.<br>
+  Docente FACI.</p><br>
+
+  <p class="nombre">Ing. Romulo Jurado  MsC.<br>
+  Docente FACI.</p><br>
+
+  <p class="nombre">Ing. Guillermo Cedeño  MsC.<br>
+  Docente FACI.</p><br>
+
+
+  <b> Decanos </b><br><br>
+  <p class="nombre">M.Sc. Luzmila María Valverde Medina.</p>
+  Facultad de la Pedagogía<br><br>
+  <p class="nombre">Ing. Segundo Nelson  Castillo Cabeza.</p>
+  Facultad de Ciencias Administrativas y Económicas.</p><br>
+  <p class="nombre">M.Sc. Iván Estupiñan Nieves.</p>
+  Facultad de Ciencias Agropecuarias<br><br>
+  <p class="nombre">Abg. Kathia Díaz Bedoya. </p>
+  Facultad de Ciencias Sociales<br><br>
+  <p class="nombre">Ing. Victor Lenin Montaño Roldan.</p>
+  Facultad de las Ingenierías<br>
+
+  </main>
+</div>
+
+<div style="float:left; width: 20%; padding-right:10px;">
+  <header>
+    <p  style="margin-bottom: 10px; margin-top:10px;" ><b>COMISIÓN DE PRESUPUESTO</b></p>
+  </header>
+  <main>
+  <p class="nombre">PhD. Juan Tacoronte.
+  <br><i>Docente FACI</i><br><br></p>
+
+  <p class="nombre">Ing. Galo Maldonado M.Sc. </p>
+  <i>Docente FACI</i><br><br>
+
+  <p class="nombre">Ing. Marco Navarrete M.Sc. </p>
+  <i>Docente FACI</i><br><br>
+
+
+  <p class="nombre">Ing. Marco Navarrete M.Sc. </p>
+  <i>Docente FACI</i><br><br>
+
+
+  <p class="nombre">M.Sc. Lucía Germania Chávez Ruano.</p>
+  <i>Pedagogía de las Ciencias Experimentales de las Matemáticas y Física.</i><br><br>
+
+  <p class="nombre">PhD. Lázaro Lima Cazorla.</p>
+  <i>Pedagogía de las Ciencias Experimentales de Química y Biología.</i> <br><br>
+
+  <p class="nombre">M.Sc. Ana Bedoya Gutierrez.</p>
+  <i>Educación Inicial.</i> <br> <br>
+   
+
+  </main>
+</div>
+
+<div style="float:left; width: 20%; padding-right:10px">
+  <header>
+    <p style="margin-bottom: 10px; margin-top:10px;" ><b>COMISIÓN DE INSCRIPCIONES Y LOGÍSTICA</b></p>
+  </header>
+
+  <main>
+  <p class="nombre">Ing. Nakira Valencia. </p>
+  <p class="nombre">Ing. Indelira Marquez.</p>
+  <p class="nombre">Ing. Stalin Francis.</p>
+  <p class="nombre">Ing. Viviana Aparicio.</p>
+  <p class="nombre">Ing. David Ordoñez.</p><br>
+  <p class="nombre">Ing. Karen Simisterra.</p><br>
+  <p class="nombre">Ing. Leticia Moreno.</p><br>
+  <p class="nombre">Ing. Martin Ramirez.</p><br>
+  <p class="nombre">Ing. Nicomedes Ordoñez.</p><br>
+  <p class="nombre">Ing. Alexander Mecias.</p><br>
+  <p class="nombre">Ing. Yuri Ramirez.</p><br>
+  <p class="nombre">Ing. Jose Luis Castillo.</p>
+  <p class="nombre">PhD. Silvia Simisterra.</p>
+  <p class="nombre">PhD. Paul Viscaino.</p>
+  <p class="nombre">PhD. Tanya Cardena.</p>
+  <p class="nombre">PhD. Narcisa Espinal.</p>
+  <p class="nombre">PhD. Damian Perez.</p>
+  <p class="nombre">PhD. Napoleon Padilla.</p>
+
+   
+
+  </main>
+</div>
+
+
+<div style="float:left; width: 20%;">
+  <header>
+    <p style="margin-bottom: 10px; margin-top:10px;" ><b>COMISIÓN DE COMUNICACÓN Y PRENSA</b></p>
+  </header>
+  <main>
+  <p class="nombre">Ing. Marcelo Reina M.Sc.<br> 
+ <i> Docente FACI.</i><br></p>
+
+
+  <p class="nombre">Ing. Yimmy Ramirez M.Sc.<br> 
+ <i> Docente FACI.</i><br></p>
+
+
+  <p class="nombre">Ing. Jonny Quiñonez  M.Sc.<br> 
+ <i> Docente FACI.</i><br></p>
+
+
+  <p class="nombre">Ing. Xavier Gracia  M.Sc.<br> 
+ <i> Docente FACI.</i><br></p>
+
+
+  <p class="nombre">Ing. Marcos Navarrete M.Sc.<br> 
+ <i> Docente FACI.</i><br></p>
+
+
+  <p class="nombre">Ing. Alejandro Macias M.Sc.<br> 
+ <i> Docente FACI.</i><br></p>
+
+
+
+  <p class="nombre">Ing. Nelson Mora M.Sc.<br> 
+ <i> Docente FACI.</i><br></p>
+
+
+  <p class="nombre">Ing. Lenin Montaño  M.Sc.<br> 
+ <i> Docente FACI.</i><br></p>
+
+
+
+  <p class="nombre">Ing. Jazmin Saltos M.Sc.<br> 
+ <i> Docente FACI.</i><br></p>
+
+
+
+
+  <b><u>Comisión de vinculación</u></b><br><br>  
+ <p style="color:gray;"> Ing.  Guillermo Mosquera.</p>
+  <p style="color:gray;"> Ing. Wilson Bedoya.</p>
+  <p style="color:gray;"> Ing. Raul Ullo.</p><br>
+  <p style="color:gray;"> Ing. Wladimir Obando.</p><br>
+  <p style="color:gray;"> Ing. Jose Luis Sampreto.</p><br>
+  <p style="color:gray;"> Ing. Franklin Reina (hijo).</p><br>
+  <p style="color:gray;"> Ing. Eduardo Rivas.</p><br>
+  <p style="color:gray;"> Ing. Silvia Simisterra.</p><br>
+  <p style="color:gray;"> Ing. Indelira Marquez.</p><br>
+  <p style="color:gray;"> Ing. Romel Mera.</p><br>
+  <p style="color:gray;"> Ing. Douglas Quiñonez.</p><br>
+
+   
+
+  </main>
+</div>
+</div>
+</div>
+
