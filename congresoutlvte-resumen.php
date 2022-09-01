e50d55abc3b14849faaa137379fede6806588b2d
Stain Francis <educaysoft@gmail.com>
2022-08-31 21:48:50 -0400

    mejoras

--- congresoutlvte-resumen.php
+++ congresoutlvte-resumen.php
@@ -1,13 +1,13 @@
 <div id="equipo" style="border-top: 2px solid green;    width: 90%; margin:20px auto; display: inline-block; ";
 <div id="xxx" class="w3-container" style=" margin-left:10px; margin-top:10px;  padding: 0px; width: 100%; height: 20vh; display: inline-block; ">
 
-<div style="float:left; width: 100%; font-size: 0.5wv;">
+<div style="float:left; width: 100%; font-size: 0.5wv;padding:10%">
   
   <main>
   
 
 
-  <p class="nombre">En el marco de sus años de creación institucional, el 12 de noviembre  de 1984, y orientada a la implementación de estrategias de desarrollo sustentable desde la multidisciplinariedad y la integración de disciplinas, la Facultad de Ingenierias, FACI de la UTLVTE se apresta a inaugurar el I Congreso Internacional "Miradas sobre las Ciencias de Ingenieros e ingenieras - FACI-UTLVTE 2022", a realizarse durante los días 7,8,9 de noviembre de 2022.</p> 
+  <p class="nombre" style="text-align:center">En el marco de sus años de creación institucional, el 12 de noviembre  de 1984, y orientada a la implementación de estrategias de desarrollo sustentable desde la multidisciplinariedad y la integración de disciplinas, la Facultad de Ingenierias, FACI de la UTLVTE se apresta a inaugurar el I Congreso Internacional "Miradas sobre las Ciencias de Ingenieros e ingenieras - FACI-UTLVTE 2022", a realizarse durante los días 7,8,9 de noviembre de 2022.</p> 
 
   </main>
 </div>
