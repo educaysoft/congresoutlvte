3c3d3fb1f91a299ea04e88d82edf89be4337237b
Stain Francis <educaysoft@gmail.com>
2022-09-02 06:46:26 -0400

    mejoras

--- congresoutlvte-album.php
+++ congresoutlvte-album.php
@@ -1,16 +1,10 @@
-  
 <div id="equipo" style="border-top: 2px solid green;    width: 90%; margin:20px auto; display: inline-block; ";
 <div id="xxx" class="w3-container" style=" margin-left:10px; margin-top:10px;  padding: 0px; width: 100%; height: 20vh; display: inline-block; ">
     
 
+<div style="float:left; width: 100%; font-size: 0.5wv;padding:10%">
 
 <main>
-
-
-    
-
-
-
 
   <div class="album py-5 bg-light">
     <div class="container">
