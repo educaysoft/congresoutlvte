7a8ae8a80e87e226cd925604573d3e3570d3b860
Stain Francis <educaysoft@gmail.com>
2022-09-02 06:24:03 -0400

    mejoras

--- congresoutlvte-album.php
+++ congresoutlvte-album.php
@@ -7,14 +7,12 @@
 <main>
 
 
-<section class="py-5 text-center container">
     <div class="row py-lg-5">
       <div class="col-lg-6 col-md-8 mx-auto">
         <h1 class="fw-light">Dirección y coordinación del congreso</h1>
         <p class="lead text-muted">Miembros de honor.</p>
       </div>
     </div>
-  </section>
 
 
 
