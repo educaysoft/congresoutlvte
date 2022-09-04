409e2b2f785d30926478193f6c4ec6de91c3e3bb
Stain Francis <educaysoft@gmail.com>
2022-09-04 11:43:22 -0400

    mejoras

--- congresoutlvte-presentacion.php
+++ congresoutlvte-presentacion.php
@@ -17,7 +17,6 @@ include("head.php");
 
 
 
-    <div class="col-md-6">
       <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
         <div class="col p-4 d-flex flex-column position-static">
           <strong class="d-inline-block mb-2 text-primary">World</strong>
@@ -28,7 +27,6 @@ include("head.php");
         </div>
         
       </div>
-    </div>
 
 
 
