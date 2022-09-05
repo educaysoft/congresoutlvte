2fc225682ce373aecef15935e96ba35f498c80de
Stain Francis <educaysoft@gmail.com>
2022-09-05 18:55:22 -0400

    mejoras

--- head.php
+++ head.php
@@ -81,7 +81,7 @@
             <h5 class="mb-6" style="color: white; text-shadow: 2px 2px #000000;">  noviembre 7,8,9 - 2022 <br> Esmeraldas-Ecuador  </h5>
  <a
             class="btn btn-outline-light btn-lg m-2"
-             href="#"
+             href="https://docs.google.com/forms/d/e/1FAIpQLSdhljELClt8TYWDNovtIRamJXMZLYsBM_SKGr1eAfWX_TrqOw/viewform?usp=sf_link"
             role="button"
             rel="nofollow"
             target="_blank"
