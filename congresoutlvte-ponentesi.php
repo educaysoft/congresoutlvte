78a701c8d67c943dc9ac6452e00e1efebe507309
Stain Francis <educaysoft@gmail.com>
2022-09-07 23:22:22 -0400

    mejoras

--- congresoutlvte-ponentesi.php
+++ congresoutlvte-ponentesi.php
@@ -19,7 +19,7 @@
       <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
         <div class="col">
           <div class="card shadow-sm">
-            <img src="http://www.congresoutlvte.org/faci/ponentes/ponente1r.jpg">
+            <img src="http://www.congresoutlvte.org/faci/ponentes/ponente1.jpg">
 
             <div class="card-body">
               <p class="card-text"><strong>Mario Heredia</strong>  <br> Miembro asociado del Centro de Estudios del Ambiente y del Mar (CESAM) de la Universidad Aveiro en Portugal<br>_</p>
@@ -34,7 +34,7 @@
         </div>
         <div class="col">
           <div class="card shadow-sm">
-            <img src="http://www.congresoutlvte.org/faci/iponentes/ponente2.jpg">
+            <img src="http://www.congresoutlvte.org/faci/ponentes/ponente2.jpg">
 
             <div class="card-body">
               <p class="card-text"><b>Hugo Romero</b><br>Docente del Programa de Doctorado en Ingeniería Industrial de la Universidad Nacional Mayor de San Marcos Perú. <br> Jefe editor de la revista biotecnología UTMACH.</p>
