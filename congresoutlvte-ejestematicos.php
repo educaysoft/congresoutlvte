68eaabaec9225198ec3e324dc6266b997b761f3f
Stain Francis <educaysoft@gmail.com>
2022-09-09 05:03:20 -0400

    mejoras

--- congresoutlvte-ejestematicos.php
+++ congresoutlvte-ejestematicos.php
@@ -20,7 +20,7 @@
  <div class="my-3 p-3 bg-body rounded shadow-sm">
     <h6 class="border-bottom pb-2 mb-0">Temáticas</h6>
     <div class="d-flex text-muted pt-3">
-      <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"/><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
+      <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"/><text x="50%" y="50%" fill="#FFFFFF" dy=".3em">1</text></svg>
 
       <p class="pb-3 mb-0 small lh-sm border-bottom">
 Sistema de monitoreo remoto, in situ y en gabinete, caracterización de variables climáticas y productivas mediante sensores y procedimientos afines. Big Data.
