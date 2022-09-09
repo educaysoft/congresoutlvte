680ecdefa9a32797c2d10ba65dd06739f6dc0876
Stain Francis <educaysoft@gmail.com>
2022-09-09 04:56:17 -0400

    mejoras

--- congresoutlvte-ejestematicos.php
+++ congresoutlvte-ejestematicos.php
@@ -23,7 +23,6 @@
       <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"/><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
 
       <p class="pb-3 mb-0 small lh-sm border-bottom">
-        <strong class="d-block text-gray-dark">1</strong>
 Sistema de monitoreo remoto, in situ y en gabinete, caracterización de variables climáticas y productivas mediante sensores y procedimientos afines. Big Data.
       </p>
     </div>
@@ -40,11 +39,23 @@ Generación energética autónoma sustentable y sostenibilidad.
       <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#6f42c1"/><text x="50%" y="50%" fill="#6f42c1" dy=".3em">32x32</text></svg>
 
       <p class="pb-3 mb-0 small lh-sm border-bottom">
-        <strong class="d-block text-gray-dark">3</strong>
 
 Automatización y control en procesos técnicos y productivos.
       </p>
     </div>
+
+
+<div class="d-flex text-muted pt-3">
+      <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill=" #33ffa9"/><text x="50%" y="50%" fill="#6f42c1" dy=".3em">32x32</text></svg>
+
+      <p class="pb-3 mb-0 small lh-sm border-bottom">
+TICs en la educación y las ciencias. Aplicación a la comunidad.
+      </p>
+    </div>
+
+
+
+
     <small class="d-block text-end mt-3">
       <a href="https://congresoutlvte.org/faci/congresoutlvte-tematicas.php">Todas las temáticas</a>
     </small>
