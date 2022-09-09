1897ff536bcdd770e689988ab84376dadc06df1a
Stain Francis <educaysoft@gmail.com>
2022-09-09 05:35:30 -0400

    mejoras

--- congresoutlvte-presentacion.php
+++ congresoutlvte-presentacion.php
@@ -48,7 +48,6 @@ include("head.php");
 
 
 
-<div class="my-3 p-3 bg-body rounded shadow-sm">
     <div class="d-flex text-muted pt-3">
       <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"/><text x="50%" y="50%" fill="#FFFFFF" dy=".3em">1</text></svg>
 
@@ -81,16 +80,6 @@ Generar alianzas estratégicas a nivel inter-institucional, gubernamental, comun
       </p>
     </div>
 
-
-
-
-
-
-
-    <small class="d-block text-end mt-3">
-      <a href="https://congresoutlvte.org/faci/congresoutlvte-tematicas.php">Todas las temáticas</a>
-    </small>
-  </div>
 
 
 
