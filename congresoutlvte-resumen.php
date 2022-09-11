e7117256117afd789a5d5aa3ac7c52b11a6a0ce9
Stain Francis <educaysoft@gmail.com>
2022-09-11 18:37:23 -0400

    mejoras

--- congresoutlvte-resumen.php
+++ congresoutlvte-resumen.php
@@ -5,7 +5,7 @@
 <div class="row">
   <div class="col-xs-6 col-md-3">
     <a href="#" class="thumbnail">
-      <img src="images/logomtci.jpg" alt="...">
+      i<img src="images/logomtci.jpg" alt="...">
     </a>
   </div>
   ...
