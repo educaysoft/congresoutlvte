1d936171b6b7a9e5c6945cde5f88dc3919abd45b
Stain Francis <educaysoft@gmail.com>
2022-09-11 19:02:29 -0400

    mejoras

--- congresoutlvte-resumen.php
+++ congresoutlvte-resumen.php
@@ -2,22 +2,28 @@
 <div id="xxx" class="w3-container" style=" margin-left:10px; margin-top:10px;  padding: 0px; width: 100%; height: 20vh; display: inline-block; ">
 
 <div style="float:left; width: 100%; font-size: 0.5wv;padding:10%">
-<div class="row">
-<center> 
- <div class="col-xs-6 col-md-3">
-    <a href="congresoutlvte-presentacion.php" class="thumbnail">
-      <img src="images/logomtci.jpg" style="width:90%; heigth:auto" alt="Miradas y Tendencias de las Ciencias Ingenieriles">
-    </a>
-  </div>
-  </center>
-</div>
+
+
+
 
   <main>
   
-
-
-  <p  style="text-align:center">En el marco de sus años de creación institucional, el 12 de noviembre  de 1984, y orientada a la implementación de estrategias de desarrollo sustentable desde la multidisciplinariedad y la integración de disciplinas, la Facultad de Ingenierias, FACI de la UTLVTE se apresta a inaugurar el I Congreso Internacional "Miradas sobre las Ciencias de Ingenieros e ingenieras - FACI-UTLVTE 2022", a realizarse durante los días 7,8,9 de noviembre de 2022.</p> 
+<div class="media">
+  <div class="media-left">
+<a href="congresoutlvte-presentacion.php" class="thumbnail">
+      <img src="images/logomtci.jpg" style="width:90%; heigth:auto" alt="Miradas y Tendencias de las Ciencias Ingenieriles">
+    </a>
+    
+  </div>
+  <div class="media-body">
+    <h4 class="media-heading">Media heading</h4>
+<p  style="text-align:center">En el marco de sus años de creación institucional, el 12 de noviembre  de 1984, y orientada a la implementación de estrategias de desarrollo sustentable desde la multidisciplinariedad y la integración de disciplinas, la Facultad de Ingenierias, FACI de la UTLVTE se apresta a inaugurar el I Congreso Internacional "Miradas sobre las Ciencias de Ingenieros e ingenieras - FACI-UTLVTE 2022", a realizarse durante los días 7,8,9 de noviembre de 2022.</p>
 <p class="lead mb-0"><a href="congresoutlvte-presentacion.php" class="stretched-link">Continue reading...</a></p>
+    ...
+  </div>
+</div>
+
+   
   </main>
 </div>
 </div>
