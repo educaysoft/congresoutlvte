22385557e374aa65db176e13c513aee795fb95cd
Stain Francis <educaysoft@gmail.com>
2022-09-11 23:28:10 -0400

    mejoras

---
+++ congresoutlvte-biografia2.php
@@ -0,0 +1,93 @@
+
+<?php
+include("head0.php");
+?>
+
+
+<?php
+include("head.php");
+?>
+
+
+<div style="width: 100%; margin:0px auto; padding: 10px;background-color: white;">
+
+
+<main  class="container" >
+<p style="font-size: 30px; font-weight: bold; text-align: center; padding: 20px 0 40px 0; "> Dr. Hugo Romero Bonilla PhD </p>
+
+<div class="text-center">
+  <img src="http://www.congresoutlvte.org/faci/ponentes/ponente2.jpg" class="rounded" alt="...">
+</div>
+
+
+	<div class="row py-lg-5 text-center" style="padding:10px; ">
+			<p class="lead text-muted">
+
+
+Hugo Romero Bonilla es Dr en Química Industrial, cuenta con Maestría en Procesamiento de Alimentos, Maestría en Gerencia y Administración de empresas Agropecuarias, y una Maestría en Ciencias Químicas de la Pontificia Universidad Católica de Valparaíso (PUCV), Chile, Cuenta con un PhD en Ciencias Ambientales en la Universidad Nacional Mayor de San Marcos(UNMSM), Perú, donde se desempeña como Docente Invitado en los Programas de Doctorado en Ingeniería industrial y Doctorado en Ciencias Ambientales. Desde hace 21 años es docente investigador titular Principal  en las Carreras de Ingeniería Química, Bioquímica y Farmacia e Ingeniería en Alimentos de la Facultad de Ciencias Químicas y de la Salud de la Universidad Técnica de Machala, Ecuador. En la  UTMACH se ha desempeñado como Director de la Carrera de Ingeniería en Alimentos, Director del Centro de Investigaciones Químicas y Tecnológicas y además como Director del Centro de Transferencia y Desarrollo de Tecnologías. Sus líneas de investigación son la Electroquímica Aplicada, Bioingeniería y el Aprovechamiento bioenergético renovable  y sostenible de Residuos. Colabora con Redes y Grupos de Investigación de Portugal, Chile, Alemania, México y Perú.  Ha publicado alrededor de 40 artículos en revistas indexadas en SCOPUS, SCIMAGO, SJR, Scielo, Redalyc. Desde el año 2010 es el Director del Consejo Editorial de la REVISTA DE BIOTECNOLOGÍA DE LA UNIVERSIDAD TÉCNICA DE MACHALA.
+
+.</p>
+    	</div>
+
+
+
+
+	
+
+
+
+	
+
+
+
+
+	
+
+
+
+
+</main>
+
+</div>
+<?php
+
+include("footer.php");
+?>
+
+
+</div>
+
+
+<!-- MDB -->
+<script
+  type="text/javascript"
+  src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.4.0/mdb.min.js"
+></script>
+
+<script type="text/javascript">
+
+$(".submenu").click(function(){
+  $(this).children("ul").slideToggle();
+})
+
+$("ul").click(function(ev){
+  ev.stopPropagation();
+})
+
+
+function cargarVideo(url){
+	document.getElementById('slider').src=url;
+}
+
+
+</script>
+    <script src="./assets/dist/js/bootstrap.bundle.min.js"></script>
+
+
+
+
+
+</html>
+
+</body>
+
