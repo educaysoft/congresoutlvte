46b18ff038c635335e6491971146e3aad29734c6
Stain Francis <educaysoft@gmail.com>
2022-09-11 23:14:53 -0400

    mejoras

---
+++ congresoutlvte-biografia1.php
@@ -0,0 +1,142 @@
+
+<?php
+include("head0.php");
+?>
+
+
+<?php
+include("head.php");
+?>
+
+
+<div style="width: 100%; margin:0px auto; padding: 10px;background-color: white;">
+
+
+<main  class="container" >
+<p style="font-size: 30px; font-weight: bold; text-align: center; padding: 20px 0 40px 0; "> PRESENTACIÓN DEL EVENTO</p>
+
+<div class="text-center">
+  <img src="http://www.congresoutlvte.org/faci/ponentes/ponente1.jpg" class="rounded" alt="...">
+</div
+
+
+	<div class="row py-lg-5 text-center ">
+        		<p class="lead text-muted">En el marco de sus años de creación institucional, el 12 de noviembre  de 1984, y orientada a la implementación de estrategias de desarrollo sustentable desde la multidisciplinariedad y la integración de disciplinas, la Facultad de Ingenierias, FACI de la UTLVTE se apresta a inaugurar el I Congreso Internacional "Miradas sobre las Ciencias de Ingenieros e ingenieras - FACI-UTLVTE 2022", a realizarse durante los días 7,8,9 de noviembre de 2022.</p>
+    	</div>
+
+
+
+
+	<div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+	<div class="row py-lg-5 text-center container">
+
+          <h3 class="fw-light">Visión del Evento</h3>
+          <p class="lead text-muted">Promover el desarrollo de la investigación multidisciplinaria en ciencias ingenieriles, y su aplicación en diferentes contextos en aras del desarrollo técnico-ingenieril eco-sustentable.</p>
+        </div>
+      </div>
+
+
+
+	<div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+	<div class="row py-lg-5 text-center container">
+          <h3 class="fw-light">Misión del evento</h3>
+          <p class="lead text-muted">Fortalecimiento de la vinculación y relaciones entre la Academia, Gobierno, Comunidad Científica UTLVTE, Instituciones, Grupos de Investigación y la Empresa Pública y Privada.</p>
+        </div>
+              </div>
+
+
+
+
+	<div class="row g-0  overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+	<div class="row py-lg-5 text-center container">
+          <h3 class="fw-light">Objetivos</h3>
+
+
+
+    <div class="d-flex text-muted pt-3">
+      <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"/><text x="50%" y="50%" fill="#FFFFFF" dy=".3em">1</text></svg>
+
+      <p class="pb-3 mb-0 small lh-sm border-bottom">
+Estimular la cultura de la investigación universitaria multidiscipinaria, y aplicada.
+      </p>
+    </div>
+    <div class="d-flex text-muted pt-3">
+      <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#e83e8c"/><text x="50%" y="50%" fill="#ffffff" dy=".3em">2</text></svg>
+
+      <p class="pb-3 mb-0 small lh-sm border-bottom">
+Debatir, críticamente, los desarrollos conceptuales, metodológicos y estados de la técnica y el arte.
+
+      </p>
+    </div>
+    <div class="d-flex text-muted pt-3">
+      <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#6f42c1"/><text x="50%" y="50%" fill="#ffffff" dy=".3em">3</text></svg>
+
+      <p class="pb-3 mb-0 small lh-sm border-bottom">
+Actualizar las tendencias de investigación en ciencias técnicas e ingenierías a nivel global, y a escala local.
+      </p>
+    </div>
+
+
+<div class="d-flex text-muted pt-3">
+      <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#33ffa9"/><text x="50%" y="50%" fill="#6f42c1" dy=".3em">4</text></svg>
+
+      <p class="pb-3 mb-0 small lh-sm border-bottom">
+Generar alianzas estratégicas a nivel inter-institucional, gubernamental, comunitario y regional.
+      </p>
+    </div>
+
+
+
+
+        </div>
+        
+      </div>
+
+
+
+
+</main>
+
+</div>
+<?php
+
+include("footer.php");
+?>
+
+
+</div>
+
+
+<!-- MDB -->
+<script
+  type="text/javascript"
+  src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.4.0/mdb.min.js"
+></script>
+
+<script type="text/javascript">
+
+$(".submenu").click(function(){
+  $(this).children("ul").slideToggle();
+})
+
+$("ul").click(function(ev){
+  ev.stopPropagation();
+})
+
+
+function cargarVideo(url){
+	document.getElementById('slider').src=url;
+}
+
+
+</script>
+    <script src="./assets/dist/js/bootstrap.bundle.min.js"></script>
+
+
+
+
+
+</html>
+
+</body>
+
