06ee95ea2ff8865815ebea9a13c6819626560471
Stain Francis <educaysoft@gmail.com>
2022-09-11 22:21:58 -0400

    mejoras

---
+++ congresoutlvte-precios.php
@@ -0,0 +1,92 @@
+<div id="equipo" style="border-top: 2px solid green;    width: 90%; margin:20px auto; display: inline-block; ";
+<div id="xxx" class="w3-container" style=" margin-left:10px; margin-top:10px;  padding: 0px; width: 100%; height: 20vh; display: inline-block; ">
+    
+
+
+<main>
+
+  <div class="album py-5 bg-light">
+    <div class="container">
+
+ <div class="row py-lg-5 text-center container">
+      <div class="col-lg-6 col-md-8 mx-auto">
+        <h1 class="fw-light">Ejes temáticos - 10 </h1>
+      </div>
+    </div>
+
+
+
+ <div class="my-3 p-3 bg-body rounded shadow-sm">
+    
+    
+    
+<table class="table table-sm">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Participantes</th>
+      <th scope="col">Cuota</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Profesionales:</td>
+      <td>90 USD.</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Estudiantes participantes:</td>
+      <td>20 USD.</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Estudiantes ponentes:</td>
+      <td>30 USD.</td>
+    </tr>
+<tr>
+      <th scope="row">4</th>
+      <td colspan="2">Personas naturales/juridicas:</td>
+      <td>90 USD.</td>
+    </tr>
+<tr>
+      <th scope="row">5</th>
+      <td colspan="2">Empresas:</td>
+      <td>90 USD.</td>
+    </tr>
+<tr>
+      <th scope="row">6</th>
+      <td colspan="2">Stands:</td>
+      <td>90 USD.</td>
+    </tr>
+
+<tr>
+      <th scope="row">7</th>
+      <td colspan="2">Invitados y participantes extranjeros:</td>
+      <td>sin costos.</td>
+    </tr>
+
+  </tbody>
+</table>
+
+
+
+
+
+
+
+
+
+    
+  </div>
+
+  
+
+
+      
+    </div>
+  </div>
+
+</main>
+
+</div>
