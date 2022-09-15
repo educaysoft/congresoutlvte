d0495df8bb08d6b71626bdda2701ac70afca397b
Stain Francis <educaysoft@gmail.com>
2022-09-14 23:07:03 -0400

    mejoras

--- head.php
+++ head.php
@@ -77,7 +77,7 @@
        <div class="d-flex justify-content-center align-items-center h-100">
           <div class="text-black">
            <h1 class="mb-3" style="color: white; text-shadow: 2px 2px #000000;">I CONGRESO INTERNACIONAL </h1>
-           <p class="mb-4 bg-success p-2 bg-opacity-10" style="color: white;font-weight:900; font-size:1.5em; text-shadow: 2px 2px #000000;" > MIRADAS Y TENDENCIAS DE LAS CIENCIAS INGENIERILES (MTCI) - UTLVTE 2022 </p>
+           <p class="mb-4 bg-success p-2 bg-opacity-25" style="color: white;font-weight:900; font-size:1.5em; text-shadow: 2px 2px #000000;" > MIRADAS Y TENDENCIAS DE LAS CIENCIAS INGENIERILES (MTCI) - UTLVTE 2022 </p>
         <p class="mb-5" style="Font-family: Impact, Charcoal, Helvetica Inserat, Bitstream Vera Sans Bold, Arial Black, sans serif; color: white; font-weight:900;font-size:1.17em;text-shadow: 2px 2px #000000;">   noviembre 7,8,9 - 2022 <br> Esmeraldas-Ecuador  </p>
  <a
             class="btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold"
