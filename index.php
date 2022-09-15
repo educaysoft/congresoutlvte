e6933db8910f25252789e87a9d70a690e8a6452b
Stain Francis <educaysoft@gmail.com>
2022-09-15 06:35:40 -0400

    mejoras

--- index.php
+++ index.php
@@ -242,6 +242,7 @@ include_once("head.php");
 
 <div style="display:flex;flex-direction: column; background-color: white;">
 <?php
+echo "hola";
 include_once("cuenta-regresiva/cuenta-regresiva.php");
 ?>
 
