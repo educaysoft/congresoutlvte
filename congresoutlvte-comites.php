eb145b7ee1cf5347137e9e658da72f96ea527ab4
ElProfe <educaysoft@gmail.com>
2022-09-25 04:45:59 -0500

    mejoras

--- congresoutlvte-comites.php
+++ congresoutlvte-comites.php
@@ -1,7 +1,7 @@
 <div id="equipo" style="border-top: 2px solid green;    width: 90%; margin:20px auto; display: inline-block; ";
 <div id="xxx" class="w3-container" style=" margin-left:10px; margin-top:10px;  padding: 0px; width: 100%; height: 20vh; display: inline-block; ">
 
-<div style="float:left; width: 25%; font-size: 0.5wv;">
+<div style="float:left; width: 25%; font-size: 0.3wv;">
   <header>
     <p style="margin-bottom: 10px; margin-top:10px;"><b>AUTORIDADES</b></p>
   </header>
