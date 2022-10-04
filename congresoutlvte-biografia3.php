f500b00ccb61057bdbff541de0ce4f00eccb6e41
Stalin Francis <educaysoft@gmail.com>
2022-10-04 11:15:45 -0500

    mejoras

---
+++ congresoutlvte-biografia3.php
@@ -0,0 +1,97 @@
+
+<?php
+include("head0.php");
+?>
+
+
+<?php
+include("head.php");
+?>
+
+
+<div style="width: 100%; margin:0px auto; padding: 10px;background-color: white;">
+
+
+<main  class="container" >
+<p style="font-size: 30px; font-weight: bold; text-align: center; padding: 20px 0 40px 0; "> Ing. Jose Mamani MSc.</p>
+<div class="text-center">
+  <img src="http://www.congresoutlvte.org/faci/ponentes/ponente2.jpg" class="img-circle"  style="width:50%" alt="Ing. Jose Mamani, MSc.">
+</div>
+
+
+
+
+	<div class="row py-lg-5 text-center" style="padding:10px; ">
+			<p class="lead text-muted">
+
+
+
+Es Ingeniero Químico de la Universidad Nacional del Altiplano de Perú y Magister en Ciencias de la Ingeniería con mención en Metalurgia Extractiva por la Universidad de Chile - Chile, es candidato a Doctor con mención en Ingeniería y Ciencias Ambientales en la Universidad Nacional Agraria la Molina de Perú. Cuenta con 5 años de experiencia profesional como docente de pre y posgrado en los cursos de transferencia de masa, operaciones y procesos químicos, tecnologías de extracción de litio y uranio, modelamiento de procesos y simulación de procesos metalúrgicos. También cuenta con 10 años de experiencia profesional en la industria minera, en el procesamiento de minerales de Plata, Oro y Cobre. El área de su especialización es la electroquímica y el procesamiento de minerales. Cuenta con investigaciones realizadas en las áreas de electrocoagulación y electroflotación aplicadas al tratamiento de aguas residuales y domésticas, así como, en las tecnologías de extracción de litio a partir de mineral de roca. Cuenta con publicaciones y libros en área de procesos químicos. Actualmente es docente de la carrera de Ingeniería Química y de la Maestría en Extracción de Metales Estratégicos de la Universidad Nacional del Altiplano de Puno, así como, consultor en temas ambientales y de procesos metalúrgicos.
+
+
+
+.</p>
+    	</div>
+
+
+
+
+	
+
+
+
+	
+
+
+
+
+	
+
+
+
+
+</main>
+
+</div>
+<?php
+
+include("footer.php");
+?>
+
+
+</div>
+
+
+<!-- MDB -->
+<script
+  type="text/javascript"
+  src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.4.0/mdb.min.js"
+></script>
+
+<script type="text/javascript">
+
+$(".submenu").click(function(){
+  $(this).children("ul").slideToggle();
+})
+
+$("ul").click(function(ev){
+  ev.stopPropagation();
+})
+
+
+function cargarVideo(url){
+	document.getElementById('slider').src=url;
+}
+
+
+</script>
+    <script src="./assets/dist/js/bootstrap.bundle.min.js"></script>
+
+
+
+
+
+</html>
+
+</body>
+
