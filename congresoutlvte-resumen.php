aa43aec9e8763a024f25989c2baff96ebb4c88bd
Stain Francis <educaysoft@gmail.com>
2022-10-07 00:46:29 -0400

    todo

--- congresoutlvte-resumen.php
+++ congresoutlvte-resumen.php
@@ -1,5 +1,5 @@
-<div id="equipo" style="border-top: 2px solid green;    width: 90%; margin:20px auto; display: inline-block; ";
-<div id="xxx" class="w3-container" style=" margin-left:10px; margin-top:10px;  padding: 0px; width: 100%; height: 20vh; display: inline-block; ">
+<div id="equipo" style="border-top: 2px solid green;    width: 90%; margin:20px auto; display: inline-block; ">
+<div id="xxx" class="w3-container" style=" margin-left:10px; margin-top:10px;  padding: 0px; width: 100%; height: 100%; display: inline-block; ">
 
 <div style="float:left; width: 100%; font-size: 0.5wv;padding:10%">
 
@@ -18,6 +18,7 @@
 
    
   </main>
+</div>
 </div>
 </div>
 
