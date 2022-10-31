c828c95d97b0616fccef67f21a12107d00966a65
Stain Francis <educaysoft@gmail.com>
2022-10-31 17:12:11 -0400

    memoria

--- head.php
+++ head.php
@@ -40,6 +40,10 @@
             <li class="nav-item">
               <a class="nav-link" href="congresoutlvte-confe2.php#">Conferencistas</a>
             </li>
+            <li class="nav-item">
+              <a class="nav-link" href="congresoutlvte-memoria.php#">Memoria</a>
+            </li>
+
             <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Repositorio
