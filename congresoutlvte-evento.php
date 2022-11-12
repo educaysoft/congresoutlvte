d2dbac23dfe3c5040024b33782c3fbf85d445184
Stain Francis <educaysoft@gmail.com>
2022-11-11 23:37:19 -0500

    mas eventos

--- congresoutlvte-evento.php
+++ congresoutlvte-evento.php
@@ -21,6 +21,24 @@ include("head.php");
 <main id="agenda" class="cd-main-content" style="margin-top:0%">
 <p style="font-size: 30px; font-weight: bold; text-align: center; padding: 20px 0 40px 0; "> TRANSMISIÓN DEL EVENTO</p>
 
+<div id="fsaludo" style="  border-top: 2px solid green; background-color:gray;   width: 100%; margin:0px auto; margin-bottom: 100px; padding-top: 50px; padding-bottom: 100px; display: flex; flex-direction:column ; ">
+
+
+<div class="delimitador">
+<div class="contenedor">
+<center>
+<iframe  id="idiframe" src="https://www.youtube.com/embed/rmiauIKa4kk"></iframe>
+</center>
+</div>
+<div class="content" >
+  <h1>Día No. -1: 04 -  Noviembre -2022  -  PRECONGRESO </h1>
+</div>
+</div>
+
+</div>
+
+
+
 
 
 <div id="fsaludo" style="  border-top: 2px solid green; background-color:gray;   width: 100%; margin:0px auto; margin-bottom: 100px; padding-top: 50px; padding-bottom: 100px; display: flex; flex-direction:column ; ">
@@ -81,8 +99,20 @@ include("head.php");
 
 
 
+<div id="fsaludo" style="  border-top: 2px solid green; background-color:gray;   width: 100%; margin:0px auto; margin-bottom: 100px; padding-top: 50px; padding-bottom: 100px; display: flex; flex-direction:column ; ">
 
+<div class="delimitador">
+<div class="contenedor">
+<center>
+<iframe id="idiframe" id="idiframe" src="https://www.youtube.com/embed/86wwyaQ70nk?autoplay=0"></iframe>
+</center>
+</div>
+<div class="content" >
+  <h1>Día No. 5: 11 - noviembre -2022 -   SESIÓN SOLEMNE </h1>
+</div>
+</div>
 
+</div>
 
 
 
